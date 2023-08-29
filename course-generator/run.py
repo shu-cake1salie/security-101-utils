6e0d0f29d5c4e7c380ccadc514730bbdcc811474
LuminolT <luminol.chen@gmail.com>
2023-08-29 17:55:36 +0800

    update: add mkdocs helper

--- course-generator/run.py
+++ course-generator/run.py
@@ -42,37 +42,59 @@ def deduce_folder_name(code: str, name: str) -> str:
         return f"{code.split('~')[0]}_{name}"
 
 
-# ----Script---------------------------------------------
+def generator():
+    """ generate the course folder and index.md file
+    """
+    os.makedirs('outputs', exist_ok=True)
 
-os.makedirs('outputs', exist_ok=True)
+    # read the template file
+    with open('template.md', 'r', encoding='utf-8') as template_file:
+        template_content = template_file.read()
 
-# read the template file
-with open('template.md', 'r', encoding='utf-8') as template_file:
-    template_content = template_file.read()
+    with open('courses.csv', 'r', encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file)
 
-with open('courses.csv', 'r', encoding='utf-8') as csv_file:
-    csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            folder_name = deduce_folder_name(row[0], row[1])
+            output_folder = os.path.join('outputs', folder_name)
+            os.makedirs(output_folder, exist_ok=True)
 
-    for row in csv_reader:
-        folder_name = deduce_folder_name(row[0], row[1])
-        output_folder = os.path.join('outputs', folder_name)
-        os.makedirs(output_folder, exist_ok=True)
-        
-        _1_course_name = row[2] # 高级语言程序设计
-        _2_course_code = row[0] # 08304135
-        _3_course_type = deduce_course_type(row[3], row[4])
-        
+            _1_course_name = row[2] # 高级语言程序设计
+            _2_course_code = row[0] # 08304135
+            _3_course_type = deduce_course_type(row[3], row[4])
 
-        filled_template = template_content\
-                            .replace('$1', _1_course_name)\
-                            .replace('$2', _2_course_code)\
-                            .replace('$3', _3_course_type)
-        
 
-        with open(os.path.join(output_folder, 'index.md'), 'w', encoding='utf-8') as index_file:
-            index_file.write(filled_template)
-        
-        print(f"Created folder: {output_folder}")
+            filled_template = template_content\
+                                .replace('$1', _1_course_name)\
+                                .replace('$2', _2_course_code)\
+                                .replace('$3', _3_course_type)
 
-print("Batch processing completed.")
 
+            with open(os.path.join(output_folder, 'index.md'), 'w', encoding='utf-8') as index_file:
+                index_file.write(filled_template)
+
+            print(f"Created folder: {output_folder}")
+
+    print("Batch processing completed.")
+
+
+def mkdocs_helper():
+    """ generate the mkdocs.yml helper
+    """
+    
+    with open('courses.csv', 'r', encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        for row in csv_reader:
+            folder_name = deduce_folder_name(row[0], row[1])
+            
+            if row[3] == '计算机':
+                print(f"- {row[2]}: 1_course_computer/{folder_name}/index.md")
+            else:
+                print(f"- {row[2]}: 2_course_security/{folder_name}/index.md")
+                
+    print("Batch processing completed.")
+
+if __name__ == '__main__':
+    generator()
+    mkdocs_helper()
